376691ce0a61fd167cba8af0e8fd81641f9e1a38
Azateikia <113917841+Azateikia@users.noreply.github.com>
2022-11-23 18:03:04 +0700

    Test1
    trying to upload my work

---
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,30 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.DriverManager;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Util.getSessionFactory();
+        Util.getConnection();
+        UserDao userDao = new UserDaoJDBCImpl();
+        UserDao userDaoHibernate = new UserDaoHibernateImpl();
+
+        userDaoHibernate.createUsersTable();
+
+        userDaoHibernate.saveUser("Zaur", "Trigulov", (byte) 28);
+        userDaoHibernate.saveUser("Nail", "Alishev", (byte) 27);
+        userDaoHibernate.saveUser("Semion", "Fomichiod", (byte) 31);
+        userDaoHibernate.saveUser("Azat", "Yulmukhametov", (byte) 30);
+
+        userDaoHibernate.removeUserById(1);
+        userDaoHibernate.getAllUsers();
+        userDaoHibernate.cleanUsersTable();
+        userDaoHibernate.dropUsersTable();
+    }
+}
